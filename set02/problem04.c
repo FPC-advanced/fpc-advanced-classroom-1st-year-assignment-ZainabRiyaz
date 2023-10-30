4db1f79e157eed986a46592bb0694bff0443f6c1
Zainab Riyaz <zainabr.bsc23@rvu.edu.in>
2023-10-30 20:35:56 +0530

    committed 10/30/2023, 8:35:55 PM

--- set02/problem04.c
+++ set02/problem04.c
@@ -2,5 +2,13 @@
 
 #include <stdio.h>
 
+int input_array_size();
+void input_array(int n, int a[n]);
+int sum_composite_numbers(int n, int a[n]);
+void output(int sum);
 
+int input_array_size()
+{
+    
+}
 
