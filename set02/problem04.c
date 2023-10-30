c514ca9df99cc097af2b9824417b5db655ce66c3
Zainab Riyaz <zainabr.bsc23@rvu.edu.in>
2023-10-30 20:24:26 +0530

    committed 10/30/2023, 8:24:25 PM

--- set02/problem04.c
+++ set02/problem04.c
@@ -42,27 +42,3 @@ int sum_composite_numbers(int n, int a[n])
     {
         int count=0;
         for(int j=1;j<=a[n];j++)
-        {
-            if(a[n]%j==0)
-            {
-                count+=1;
-            }
-        }
-        if (count>2)
-        {
-            sum+=a[n];
-        }
-    }
-    return sum;
-}
-
-
-void output(int sum)
-{
-    printf("The sum of composite numbers in the given array is %d",sum);
-}
-
-
-
-
-
