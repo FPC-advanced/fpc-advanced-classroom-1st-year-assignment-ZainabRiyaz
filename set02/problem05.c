e7b4669f65a0fb471c0e31ba139f35d1b3ed5cac
Zainab Riyaz <zainabr.bsc23@rvu.edu.in>
2023-10-30 20:52:05 +0530

    committed 10/30/2023, 8:52:03 PM

--- set02/problem05.c
+++ set02/problem05.c
@@ -1,2 +1,6 @@
 /*Write a program to find GCD _(HCF)_ of two numbers.*/
 
+int input_array_size();
+void input_array(int n, int a[n]);
+int sum_composite_numbers(int n, int a[n]);
+void output(int sum);
