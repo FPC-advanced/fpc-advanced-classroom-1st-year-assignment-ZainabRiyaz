da11f707f7f02c49171940e0d9d8a0fad11c6f09
Zainab Riyaz <zainabr.bsc23@rvu.edu.in>
2023-10-30 20:39:28 +0530

    committed 10/30/2023, 8:39:25 PM

--- set02/problem04.c
+++ set02/problem04.c
@@ -28,11 +28,12 @@ int sum_composite_numbers(int n, int a[n])
 {
     for(int i=0;i<n;i++)
     {
+        int count=0;
         for(int j=1;j<=a[j];j++)
         {
             if(a[i]%j==0)
             {
-                
+                count+=1;
             }
         }
     }
