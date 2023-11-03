160b6ab912552567219ba52cfe68c73492df7bfb
Zainab Riyaz <zainabr.bsc23@rvu.edu.in>
2023-11-03 22:20:07 +0530

    committed 11/3/2023, 10:20:05 PM

--- set01/problem09.c
+++ set01/problem09.c
@@ -27,7 +27,7 @@ float square_root(float n)
 {
     float accy=0.0001;
     float in_guess=1.0;
-    while(n-in_guess*in_guess>accy)
+    while(in_guess*in_guess-n>accy)
     {
         in_guess=(in_guess+ (n/in_guess))/2;
     }
