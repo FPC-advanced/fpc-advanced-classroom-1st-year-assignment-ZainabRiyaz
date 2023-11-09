9705c88b7e9135176f57c8b8438bb8c9c1459ce1
Zainab Riyaz <zainabr.bsc23@rvu.edu.in>
2023-11-09 16:06:47 +0530

    committed 11/9/2023, 4:06:45 PM

--- set01/problem02.c
+++ set01/problem02.c
@@ -6,7 +6,7 @@ int main()
     int num1,num2,sum=0;
     printf("Please enter the first number: ",&num1);
     scanf("%d",&num1);
-    printf("Please enter the first number: ",&num2);
+    printf("Please enter the second number: ",&num2);
     scanf("%d",&num2);
     sum=num1+num2;
     printf("The sum is %d",sum);
