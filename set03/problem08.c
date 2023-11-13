c3d2dc6a10dc43ca10dd3f5542bea88206779b64
Zainab Riyaz <zainabr.bsc23@rvu.edu.in>
2023-11-13 20:00:53 +0530

    committed 11/13/2023, 8:00:52 PM

--- set03/problem08.c
+++ set03/problem08.c
@@ -18,59 +18,68 @@ int input_n();
 Point input_point();
 void input_polygon(Polygon *p);
 float find_distance(Point a, Point b);
-void find_perimeter(Polygon* p);
+void find_perimeter(Polygon *p);
 void output(Polygon p);
 
 int main()
 {
     Polygon pl;
     input_polygon(&pl);
-    float distance;
-    distance=find_distance(pl.p[0],pl.p[1]);
     find_perimeter(&pl);
     output(pl);
+    return 0;
 }
 
 int input_n()
 {
-    int sides;
+    int n;
     printf("Please input the number of sides of the polygon: ");
-    scanf("%d",&sides);
-    return sides;
+    scanf("%d",&n);
+    return n;
 }
 
 Point input_point()
 {
     Point p;
-    printf("Please input the x coordinate : ");
-    scanf("%f",&p.x);
-    printf("Please input the y coordinate : ");
-    scanf("%f",&p.y);
-    return p;   
+    printf("Please enter the x and y coordinte (x,y) : ");
+    scanf("%f %f",&p.x,&p.y);
+    return p;
 }
 
 void input_polygon(Polygon *p)
 {
-    p->sides=input_n();
-    p->p[0]=input_point();
-    p->p[1]=input_point();
+    int n=input_n();
+    for(int i=0;i<n;i++)
+    {
+        p->p[i]=input_point();
+    }
 }
 
 float find_distance(Point a, Point b)
 {
     float distance;
-    distance = sqrt((b.x-a.x)*(b.x-a.x)+(b.y-a.y)*(b.y-a.y));
+    distance=sqrt((b.x-a.x)*(b.x-a.x)+(b.y-a.y)*(b.y-a.y));
     return distance;
 }
 
-void find_perimeter(Polygon *p)
+void find_perimeter(Polygon* p)
 {
     float distance;
-    distance=find_distance(p->p[0],p->p[1]);
-    p->perimeter=p->sides*distance;
+    for(int i=0;i<p->sides;i++)
+    {
+        if(i!=p->sides-1)
+        {
+        distance+=find_distance(p->p[i],p->p[i+1]);
+        }
+        else if(i==p->sides-1)
+        {
+            distance+=find_distance(p->p[i],p->p[0]);
+        }
+    }
+    p->perimeter=distance;
 }
 
 void output(Polygon p)
 {
-    printf("The perimeter of the polygon is: %d ",p.perimeter);
+    printf("The perimeter is %f",p.perimeter);
 }
