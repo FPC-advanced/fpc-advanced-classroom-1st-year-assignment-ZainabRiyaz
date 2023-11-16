6c71a93a8379c9eaf12c40462190a92641d137f1
Zainab Riyaz <zainabr.bsc23@rvu.edu.in>
2023-11-16 23:33:06 +0530

    committed 11/16/2023, 11:33:05 PM

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,44 +1,45 @@
 //10. Write a C program to compare two strings, character by character.//
 
 #include <stdio.h>
+#include <string.h>
 
 void input_two_strings(char *string1, char *string2);
 int stringcompare(char *string1, char *string2);
 void output(char *string1, char *string2, int result);
 
-
 int main()
 {
     char str1,str2;
-    int result=0;
     input_two_strings(&str1,&str2);
-    result = stringcompare(&str1,&str2);
+    int result;
+    result=stringcompare(&str1,&str2);
     output(&str1,&str2,result);
     return 0;
 }
 
-void input_two_strings(char *string1,char *string2)
+void input_two_strings(char *string1, char *string2)
 {
-    printf("Input the first string: ");
+    printf("Please enter the first string: ");
     scanf("%s",string1);
-    printf("Input the second string: ");
+    printf("Please enter the second string: ");
     scanf("%s",string2);
 }
 
 int stringcompare(char *string1, char *string2)
 {
-    int i=0,flag=0;
-    while(string1[i]!='\0' || string2[i]!='\0')
+    int flag=0;
+    for(int i=0,j=0;i<strlen(string1),j<strlen(string2);i++,j++)
     {
-        if (string1[i]>string2[i])
+        if (string1[i]>string2[j])
         {
             flag=1;
         }
-        else if (string1[i]<string2[i])
+        else if (string1[i]<string2[j])
         {
-            flag=2;
+            flag=2;   
         }
-        else{
+        else if (string1[i]==string2[j])
+        {
             flag=0;
         }
     }
@@ -49,15 +50,15 @@ void output(char *string1, char *string2, int result)
 {
     if (result==1)
     {
-        printf("The greater string is %s",string1);
+        printf("%s is greater than %s",string1,string2);
     }
     else if (result==2)
     {
-       printf("The greater string is %s",string2); 
+        printf("%s is greater than %s",string2,string1);
     }
     else
     {
         printf("The strings are equal");
-    } 
+    }
+    
 }
-
