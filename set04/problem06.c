f03d78e2619be63aa351d0607174814680c5c2a3
Zainab Riyaz <zainabr.bsc23@rvu.edu.in>
2023-11-19 03:10:57 +0530

    committed 11/19/2023, 3:10:55 AM

---
+++ set04/problem06.c
@@ -0,0 +1,43 @@
+/*6. Write a program to count the number of words in a string using strtok (_string.h_)*/
+
+#include <stdio.h>
+#include <string.h>
+
+void input_string(char *a);
+int count_words(char *string);
+void output(char *string, int no_words);
+
+int main()
+{
+    char str;
+    int nword;
+    input_string(&str);
+    nword=count_words(&str);
+    output(&str,nword);
+    return 0;
+}
+
+void input_string(char *a)
+{
+    printf("Please enter the string: ");
+    scanf("%s",a);
+}
+
+int count_words(char *string)
+{
+    char tok;
+    char s=" ";
+    int count=1;
+    strtok(string,s);
+    while(tok!=NULL)
+    {
+        tok=strtok(string,s);
+        count+=1;
+    }
+    return count;
+}
+
+void output(char *string, int no_words)
+{
+    printf("The total number of words is %d",no_words);
+}
