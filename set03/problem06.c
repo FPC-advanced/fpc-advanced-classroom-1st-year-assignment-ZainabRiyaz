a98fa4f8e4eff865372a5a09914edddd2b31d471
Zainab Riyaz <zainabr.bsc23@rvu.edu.in>
2023-11-26 18:37:38 +0530

    committed 11/26/2023, 6:37:36 PM

--- set03/problem06.c
+++ set03/problem06.c
@@ -17,7 +17,7 @@ int main()
     return 0;
 }
 
-void input_string(char* a, char* b)
+void input_string(char *a, char *b)
 {
     printf("Input the string: ");
     scanf("%s",a);
@@ -25,7 +25,7 @@ void input_string(char* a, char* b)
     scanf("%s",b);
 }
 
-int sub_str_index(char* string, char* substring)
+int sub_str_index(char *string, char *substring)
 {
     int check=0,index=0,i=0;
     for(i=0;i<strlen(string);i++)
