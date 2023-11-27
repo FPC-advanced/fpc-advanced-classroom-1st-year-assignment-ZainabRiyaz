88953b0c91f90bc82ff142c855a3027805ef27a1
Zainab Riyaz <zainabr.bsc23@rvu.edu.in>
2023-11-27 22:22:53 +0530

    committed 11/27/2023, 10:22:52 PM

--- set03/problem05.c
+++ set03/problem05.c
@@ -46,20 +46,13 @@ void erotosthenes_sieve(int n, int a[n+1])
             }
         }
     }
-    for(int i=2;i<n;i++)
-    {
-        if(a[i]==0)
-        {
-            printf("%d",i);
-        }
-    }
 }
 
 void output(int n, int a[n+1])
 {
     for(int i=2;i<n;i++)
     {
-        if(a[i]==0)
+        if(a[i]!=0)
         {
             printf("%d",i);
         }
